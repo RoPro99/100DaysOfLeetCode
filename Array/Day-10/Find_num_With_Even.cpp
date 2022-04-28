ff1c0fa8060710fec1a7bea029b8f7628706812a
RoPro99 <rohankodag499@gmail.com>
2022-04-28 23:44:30 +0530

    Find Numbers with Even Digit

---
+++ Array/Day-10/Find_num_With_Even.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	int arr[1000];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	int cnt = 0;
+	for (int i = 0; i < n; i++) {
+		int Noofdigit = 0;
+		while (arr[i] != 0) {
+			Noofdigit++;
+			arr[i] /= 10;
+
+		}
+		cout << Noofdigit << endl;
+		if (Noofdigit % 2 == 0) {
+			cnt++;
+		}
+	}
+	cout << cnt << endl;
+
+
+	return 0;
+}

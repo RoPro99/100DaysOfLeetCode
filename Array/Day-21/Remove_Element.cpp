6082a0fd294998505d6c51364520f5cfab19e0fb
RoPro99 <rohankodag499@gmail.com>
2022-05-18 23:21:06 +0530

    Remove Element

---
+++ Array/Day-21/Remove_Element.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+
+	int arr[1000];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	int val;
+	cin >> val;
+	int cnt = 0;
+	for (int i = 0; i < n; i++) {
+		if (arr[i] != val) {
+			arr[cnt++] = arr[i];
+		}
+	}
+	cout << cnt << endl;
+	return 0;
+
+}

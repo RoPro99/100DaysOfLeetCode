e22d0cfbf8a7af95e8a1a6eb84721ea84538a8d3
RoPro99 <rohankodag499@gmail.com>
2022-06-02 11:20:30 +0530

    Sort Colors

---
+++ Array/Day-39/Sort_Colors.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+void dnf_sort(int *arr, int n) {
+	//Sort 0's 1's and 2's
+	int low = 0;
+	int high = n - 1;
+	int mid = 0;
+	while (mid <= high) {
+		if (arr[mid] == 0) {
+			swap(arr[mid], arr[low]);
+			low++;
+			mid++;
+		}
+		else if (arr[mid] == 1) {
+			mid++;
+		}
+		else if (arr[mid] == 2) {
+			swap(arr[mid], arr[high]);
+			high--;
+		}
+	}
+
+
+}
+
+
+int main() {
+	int n;
+	cin >> n;
+	int arr[100000];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	dnf_sort(arr, n);
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ,";
+	}
+	return 0;
+}

5c0e721cf98e6b9db522a087601882b6293bf74f
RoPro99 <rohankodag499@gmail.com>
2022-09-19 09:38:15 +0530

    Contain Duplicate

---
+++ Array/Day-59/AnotherApporach.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+bool contain(int *arr, int n) {
+
+
+	int result = false;
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n ; j++) {
+
+			if (arr[i] == arr[j]) {
+				result = true;
+
+			}
+
+
+		}
+	}
+	return result;
+
+
+
+}
+int main() {
+	int n;
+	cin >> n;
+	int arr[1000];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	if (contain(arr, n)) {
+		cout << "True" << endl;
+	}
+	else {
+		cout << "False" << endl;
+	}
+
+
+	return 0;
+}

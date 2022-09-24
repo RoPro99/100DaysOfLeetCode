6944b7d4fa30e13f6345bdbddafe19e88adb054b
RoPro99 <rohankodag499@gmail.com>
2022-09-24 19:43:57 +0530

    Find Duplicate in Array

---
+++ Array/Day-70/Find_Duplicate_Array.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int n;
+int arr[1000];
+cin>>n;
+for(int i=0;i<n;i++){
+cin>>arr[i];
+}
+sort(arr,arr+n);
+for(int i=0;i<n;i++){
+for(int j=i+1;j<n;j++){                                 
+if(arr[i]==arr[j]){
+cout<<arr[i]<<" ";
+}
+}
+}
+return 0;
+}
+
+ 

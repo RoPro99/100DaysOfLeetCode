6c1341ddf85e2feca34576e811e9301cc66365a7
RoPro99 <rohankodag499@gmail.com>
2022-09-27 09:50:34 +0530

    Find Duplicate Array

--- Array/Day-70/Find_Duplicate_Array.cpp
+++ Array/Day-70/Find_Duplicate_Array.cpp
@@ -5,16 +5,16 @@ int n;
 int arr[1000];
 cin>>n;
 for(int i=0;i<n;i++){
-cin>>arr[i];
+     cin>>arr[i];
 }
 sort(arr,arr+n);
-for(int i=0;i<n;i++){
-for(int j=i+1;j<n;j++){                                 
-if(arr[i]==arr[j]){
-cout<<arr[i]<<" ";
-}
-}
-}
+   for(int i=0;i<n;i++){
+          for(int j=i+1;j<n;j++){                                 
+               if(arr[i]==arr[j]){
+                    cout<<arr[i]<<" ";
+                }
+          }
+     }
 return 0;
 }
 

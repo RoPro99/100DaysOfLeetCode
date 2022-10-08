90f589d59098b377844805b46f5b7e245dc2a9ab
RoPro99 <rohankodag499@gmail.com>
2022-10-08 10:43:40 +0530

    Sum Of Digit in Base K

---
+++ Array/Day-94/Sum_Of_Digit_base_K.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	int k;
+	cin>>k;
+	int sum=0;
+	while(n>0){
+		sum+=(n%k);
+		n=n/k;
+	}
+	cout<<sum<<endl;
+
+return 0;	
+}
